aa08ef37d97d3326f03824937050dc09e8ec87b0
iman <iman.luensmann@gmail.com>
2020-04-06 11:22:31 +0200

    adventure game with two maps, and validating user input

--- adventure/src/ch/css/iman/Location.java
+++ adventure/src/ch/css/iman/Location.java
@@ -11,7 +11,8 @@ public class Location {
     public Location(int locationID, String description) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        this.exits = new HashMap<>();
+        this.exits.put("Q", 0);
     }
 
     public void addExit(String direction, int location) {
--- adventure/src/ch/css/iman/Main.java
+++ adventure/src/ch/css/iman/Main.java
@@ -9,14 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
-
+        setUpLocations();
+        addExits();
 
         int loc = 1;
         while (true) {
@@ -24,10 +18,73 @@ public class Main {
             if (loc == 0) {
                 break;
             }
-            loc = scanner.nextInt();
-            if (!locations.containsKey(loc)) {
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exits are ");
+            for (String e : exits.keySet()) {
+                System.out.print(e + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+
+            if (direction.getBytes().length != 1) {
+                direction = validateUserInput(direction);
+            }
+
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
                 System.out.println("You cannot go in that direction");
             }
         }
+    }
+
+    private static String validateUserInput(String userInput) {
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("NORTH", "N");
+
+        String[] userInputInWords = userInput.split(" ");
+
+        for (String word : userInputInWords) {
+            String upperCaseWord = word.toUpperCase();
+
+            for (String key : vocabulary.keySet()) {
+                if (key.equals(upperCaseWord)) {
+                    return vocabulary.get(key);
+                }
+            }
+        }
+        return "";
+    }
+
+    private static void addExits() {
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+    }
+
+    private static void setUpLocations() {
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location(2, "You are at the top of a hill"));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4, "You are in a valley beside a stream"));
+        locations.put(5, new Location(5, "You are in the forest"));
     }
 }
