3e8a64978295a0c44cc922423e99f8fc17343fac
iman <iman.luensmann@gmail.com>
2020-04-07 16:56:30 +0200

    example of overwriting .equals in a subclass

---
+++ SetAndHashSet/src/ch/css/iman/Labrador.java
@@ -0,0 +1,25 @@
+package ch.css.iman;
+
+public class Labrador extends Dog {
+
+    public Labrador(String name) {
+        super(name);
+    }
+
+    //it gets an error bc the base class .equals() is final
+/*
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof Labrador) {
+            String objName = ((Labrador) obj).getName();
+            return this.getName().equals(objName);
+        }
+        return false;
+
+    }
+*/
+
+}
