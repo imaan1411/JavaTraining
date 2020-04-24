a560fbd5efb10367927144e194883b278d4018a7
iman <iman.luensmann@gmail.com>
2020-04-24 11:15:24 +0200

    set up Employee class and fill with data

--- lambdas/src/ch/css/iman/Main.java
+++ lambdas/src/ch/css/iman/Main.java
@@ -1,8 +1,65 @@
 package ch.css.iman;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new Thread(()-> {
+            System.out.println("hello");
+            System.out.println("hell2o");
+        }).start();
+
+        Employee iman = new Employee("Iman", 20);
+        Employee sonja = new Employee("Sonja", 21);
+        Employee laurin = new Employee("Laurin", 19);
+        Employee mena = new Employee("Mena", 19);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(iman);
+        employees.add(sonja);
+        employees.add(laurin);
+        employees.add(mena);
+
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        for (Employee e : employees) {
+            System.out.println(e.getName());
+        }
     }
 }
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
